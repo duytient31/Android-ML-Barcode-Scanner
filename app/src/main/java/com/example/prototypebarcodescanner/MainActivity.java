2eb2f0a77e8e58f85173e2b7ff4de75de70fe425
lewisgarton <lewisgarton@gmail.com>
2020-02-05 23:07:59 +1300

    implemented camera preview for UI textureView using CameraX

--- app/src/main/java/com/example/prototypebarcodescanner/MainActivity.java
+++ app/src/main/java/com/example/prototypebarcodescanner/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.prototypebarcodescanner;
-
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
 
+import android.util.Size;
 import android.view.TextureView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,23 +21,25 @@ import android.widget.TextView;
  * @author Lewis Garton
  * @version 1.0
  * @since 2020-02-05
- *
  */
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextureView textureView;
     private TextView textView;
-
+    private Boolean isCameraSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
         textureView = findViewById(R.id.textureView);
+        SurfaceTexture surfaceTexture = new SurfaceTexture(1);
+        textureView.setSurfaceTexture(surfaceTexture);
+        textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
 
+        setupCamerax();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +47,35 @@ public class MainActivity extends AppCompatActivity {
                 //TODO: Do something here!
             }
         });
+    }
 
+    /**
+     * Bind the camera instance and preview use-case
+     * This should be called once as camerax binds cameras and use-cases to a lifecycle Owner
+     */
+    protected void setupCamerax() {
+        if (isCameraSet) return;
+        isCameraSet = true;
+        CameraX.unbindAll();
 
+        //Create preview use-case
+        Size textureViewResolution = new Size(textureView.getWidth(), textureView.getHeight());
+        PreviewConfig previewConfig = new PreviewConfig.Builder()
+                .build();
+        Preview preview = new Preview(previewConfig);
+
+        preview.setOnPreviewOutputUpdateListener(new Preview.OnPreviewOutputUpdateListener() {
+            @Override
+            public void onUpdated(@NonNull Preview.PreviewOutput output) {
+                textureView.setSurfaceTexture(output.getSurfaceTexture());
+            }
+        });
+
+        CameraX.bindToLifecycle(this, preview);
+
+        /*
+        //TODO: Implement for debugging
+        previewBuilder.setTargetName();
+        */
     }
 }
